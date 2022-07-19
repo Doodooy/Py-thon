1d3c986d09a958663c5ce3ec433124a735ccee88
Doodooy <Blastertbest@gmail.com>
2022-07-19 23:00:08 +0530

    he he

--- swg.py
+++ swg.py
@@ -1,21 +1,58 @@
 # Made by aryaman :)
+
+
 import random
 import time
+
+
 print('Welcome to the Snake and Water game!')
-t1 = int(input('Your Turn: \n 1. Snake \n 2. Water \n 3. Gun\n-> '))
+
+time.sleep(.5)
+
+
+print('Press ENTER to begin')
+
+input('')
+
+time.sleep(1)
+
+# t1 => input from user
+# t2 => input from computer (random)
+ 
+t1 = int(input('Your Turn: \n 1. Snake \n 2. Water \n 3. Gun\n\n-> '))
 t2 = random.randint(1, 3)
+
+# Winning/losing/tie statement
+
 win_plyr = 'You win!'
 
 win_comp = 'Computer wins!'
 
 tie = 'It\'s a tie!'
 
+# Game function
 
 def game(a,b):
-    print(f'\nYou chose {a}\n')
-    time.sleep(.5)
-    print(f'The computer chose {b}\n')
-    time.sleep(.5)
+    
+    if a == 1:
+        option_plyr = 'Snake'
+    elif a == 2:
+        option_plyr = 'Water'
+    elif a == 3:
+        option_plyr = 'Gun'
+    
+    if b == 1:
+        option_comp = 'Snake'
+    elif b == 2:
+        option_comp = 'Water'
+    elif b == 3:
+        option_comp = 'Gun'
+    
+    print(f'\nYou chose {option_plyr}\n')
+    time.sleep(1)
+    print(f'The computer chose {option_comp}\n')
+    time.sleep(1)
+    
     if a == b:
         print(tie)
     elif a == 1 and b == 2:
@@ -30,7 +67,9 @@ def game(a,b):
         print(win_plyr)
     elif a == 1 and b == 3:
         print(win_plyr)
+    
     else:
         print('Please choose an option using 1,2 or 3')
+
 
 game(t1,t2)
